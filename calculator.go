1a266fc71ca4fdf98839a0f061d44eb17252ed30
jatintalgotra-zd <jatin.talgotra@zop.dev>
2025-06-16 16:00:55 +0530

    Update calculator.go - Add input error handling

--- calculator.go
+++ calculator.go
@@ -20,18 +20,24 @@ func divide(a, b int64) (float64, error) {
 func main() {
 	var a [2]int64
 
+	fmt.Println("Input 2 numbers: ")
+
 	n, err := fmt.Scan(&a[0], &a[1])
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("items read: ", n)
+	if n != 2 {
+		fmt.Println("Invalid No of Inputs")
+		return
+	}
+
 	fmt.Println("Addition: ", add(a[0], a[1]))
 
 	div, err := divide(a[0], a[1])
 	if err != nil {
-		fmt.Println("Error occured: ", err)
+		fmt.Println("Error occurred: ", err)
 	} else {
 		fmt.Println("Division: ", div)
 	}
