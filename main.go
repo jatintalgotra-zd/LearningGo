46743695d43cde0e8956e68cb8ff0faf91799f33
jatintalgotra-zd <jatin.talgotra@zop.dev>
2025-06-17 10:58:11 +0530

    merged pr's

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 // Task
@@ -25,11 +23,11 @@ func GenId() func() int {
 
 // Check (e error)
 // handle error - prints the error message from argument
-func Check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+//func Check(err error) {
+//	if err != nil {
+//		panic(err)
+//	}
+//}
 
 // AddTask (description string, nextID func() int, mp map[int]*Task)
 // adds new Task by generating id
@@ -58,57 +56,57 @@ func CompleteTask(id int, mp map[int]*Task) {
 	t1.Status = true
 }
 
-func main() {
-
-	// map used to store tasks
-	mp := make(map[int]*Task)
-	// function to generate next id
-	nextId := GenId()
-
-	// using for to loop over user inputs
-	for {
-		// menu style input formatting
-		fmt.Println("Input 1 to add Task, 2 to list pending Task, 3 to complete Task, 0 to quit")
-		var n int
-		_, err := fmt.Scan(&n)
-		Check(err)
-
-		// close program
-		if n == 0 {
-			break
-		}
-
-		// switch case to call functions
-		switch n {
-
-		// enter new Task
-		case 1:
-			fmt.Println("Enter Task: ")
-			var t1 string
-			bufReader := bufio.NewReader(os.Stdin)
-			t1, err2 := bufReader.ReadString('\n')
-			Check(err2)
-			AddTask(t1, nextId, mp)
-
-		// list pending tasks
-		case 2:
-			fmt.Println("Pending Task: ")
-			ListTasks(mp)
-
-		// make Task complete by id
-		case 3:
-			fmt.Println("Enter Task id: ")
-			var id int
-			_, err3 := fmt.Scan(&id)
-			Check(err3)
-			_, ok := mp[id]
-			if ok {
-				fmt.Println("Marking task", id, "as completed...")
-				CompleteTask(id, mp)
-			} else {
-				fmt.Println("Invalid task ID")
-			}
-		}
-	}
-
-}
+//func main() {
+//
+//	// map used to store tasks
+//	mp := make(map[int]*Task)
+//	// function to generate next id
+//	nextId := GenId()
+//
+//	// using for to loop over user inputs
+//	for {
+//		// menu style input formatting
+//		fmt.Println("Input 1 to add Task, 2 to list pending Task, 3 to complete Task, 0 to quit")
+//		var n int
+//		_, err := fmt.Scan(&n)
+//		Check(err)
+//
+//		// close program
+//		if n == 0 {
+//			break
+//		}
+//
+//		// switch case to call functions
+//		switch n {
+//
+//		// enter new Task
+//		case 1:
+//			fmt.Println("Enter Task: ")
+//			var t1 string
+//			bufReader := bufio.NewReader(os.Stdin)
+//			t1, err2 := bufReader.ReadString('\n')
+//			Check(err2)
+//			AddTask(t1, nextId, mp)
+//
+//		// list pending tasks
+//		case 2:
+//			fmt.Println("Pending Task: ")
+//			ListTasks(mp)
+//
+//		// make Task complete by id
+//		case 3:
+//			fmt.Println("Enter Task id: ")
+//			var id int
+//			_, err3 := fmt.Scan(&id)
+//			Check(err3)
+//			_, ok := mp[id]
+//			if ok {
+//				fmt.Println("Marking task", id, "as completed...")
+//				CompleteTask(id, mp)
+//			} else {
+//				fmt.Println("Invalid task ID")
+//			}
+//		}
+//	}
+//
+//}
