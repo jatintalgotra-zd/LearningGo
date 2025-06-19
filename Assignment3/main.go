f97d078d1f4e7f84aaf2f2647537c28bab323f82
jatintalgotra-zd <jatin.talgotra@zop.dev>
2025-06-19 11:27:17 +0530

    Update main branch - added square shape to 5.1_Area, updates methods for testing in Assignment3

--- Assignment3/main.go
+++ Assignment3/main.go
@@ -1,7 +1,9 @@
-package Assignment3
+package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 // Task
@@ -23,90 +25,97 @@ func GenId() func() int {
 
 // Check (e error)
 // handle error - prints the error message from argument
-//func Check(err error) {
-//	if err != nil {
-//		panic(err)
-//	}
-//}
+func Check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 // AddTask (description string, nextID func() int, mp map[int]*Task)
 // adds new Task by generating id
-func AddTask(description string, nextID func() int, mp map[int]*Task) {
+func AddTask(description string, nextID func() int, tasks *[]Task) int {
 	id := nextID()
 	t1 := Task{Desc: description}
-	mp[id] = &t1
+	*tasks = append(*tasks, t1)
 	fmt.Println("Task added:", id, "-", description)
-
+	return id
 }
 
 // ListTasks
 // prints all pending tasks
-func ListTasks(mp map[int]*Task) {
-	for k, v := range mp {
+func ListTasks(tasks *[]Task) []string {
+	taskList := make([]string, 0)
+	for k, v := range *tasks {
 		if !v.Status {
-			fmt.Println(k, ". ", v.Desc)
+			t := fmt.Sprintln(k+1, ". ", v.Desc)
+			taskList = append(taskList, t)
 		}
 	}
+	return taskList
 }
 
 // CompleteTask
 // marks Task complete by id
-func CompleteTask(id int, mp map[int]*Task) {
-	t1 := mp[id]
-	t1.Status = true
+func CompleteTask(id int, tasks *[]Task) {
+	idx := id - 1
+	if idx >= 0 && idx < len(*tasks) {
+		fmt.Println("Marking task", id, "as completed...")
+		(*tasks)[idx].Status = true
+
+	} else {
+		fmt.Println("Invalid task ID")
+	}
+
 }
 
-//func main() {
-//
-//	// map used to store tasks
-//	mp := make(map[int]*Task)
-//	// function to generate next id
-//	nextId := GenId()
-//
-//	// using for to loop over user inputs
-//	for {
-//		// menu style input formatting
-//		fmt.Println("Input 1 to add Task, 2 to list pending Task, 3 to complete Task, 0 to quit")
-//		var n int
-//		_, err := fmt.Scan(&n)
-//		Check(err)
-//
-//		// close program
-//		if n == 0 {
-//			break
-//		}
-//
-//		// switch case to call functions
-//		switch n {
-//
-//		// enter new Task
-//		case 1:
-//			fmt.Println("Enter Task: ")
-//			var t1 string
-//			bufReader := bufio.NewReader(os.Stdin)
-//			t1, err2 := bufReader.ReadString('\n')
-//			Check(err2)
-//			AddTask(t1, nextId, mp)
-//
-//		// list pending tasks
-//		case 2:
-//			fmt.Println("Pending Task: ")
-//			ListTasks(mp)
-//
-//		// make Task complete by id
-//		case 3:
-//			fmt.Println("Enter Task id: ")
-//			var id int
-//			_, err3 := fmt.Scan(&id)
-//			Check(err3)
-//			_, ok := mp[id]
-//			if ok {
-//				fmt.Println("Marking task", id, "as completed...")
-//				CompleteTask(id, mp)
-//			} else {
-//				fmt.Println("Invalid task ID")
-//			}
-//		}
-//	}
-//
-//}
+func main() {
+
+	// map used to store tasks
+	tasks := make([]Task, 0)
+	// function to generate next id
+	nextId := GenId()
+
+	// using for to loop over user inputs
+	for {
+		// menu style input formatting
+		fmt.Println("Input 1 to add Task, 2 to list pending Task, 3 to complete Task, 0 to quit")
+		var n int
+		_, err := fmt.Scan(&n)
+		Check(err)
+
+		// close program
+		if n == 0 {
+			break
+		}
+
+		// switch case to call functions
+		switch n {
+
+		// enter new Task
+		case 1:
+			fmt.Println("Enter Task: ")
+			var t1 string
+			bufReader := bufio.NewReader(os.Stdin)
+			t1, err2 := bufReader.ReadString('\n')
+			Check(err2)
+			AddTask(t1, nextId, &tasks)
+
+		// list pending tasks
+		case 2:
+			fmt.Println("Pending Task: ")
+			pendingTasks := ListTasks(&tasks)
+			for id, task := range pendingTasks {
+				fmt.Println(id, task)
+			}
+
+		// make Task complete by id
+		case 3:
+			fmt.Println("Enter Task id: ")
+			var id int
+			_, err3 := fmt.Scan(&id)
+			Check(err3)
+			CompleteTask(id, &tasks)
+		}
+	}
+
+}
--- Assignment5/5.1_Area/main.go
+++ Assignment5/5.1_Area/main.go
@@ -35,6 +35,14 @@ func (c Circle) Area() float64 {
 	return c.radius * c.radius * math.Pi
 }
 
+type Square struct {
+	side float64
+}
+
+func (s Square) Area() float64 {
+	return s.side * s.side
+}
+
 func main() {
 	r := Rectangle{12, 34}
 	c := Circle{10}
@@ -47,4 +55,28 @@ func main() {
 // function which takes a Shape argument and returns its Area
 func CalculateArea(shape Shape) float64 {
 	return shape.Area()
+}
+
+// Cost
+// function with argument of type Shape which returns cost based on Area and rates of each type
+func Cost(s Shape) float64 {
+	// 0.02 for sq, 0.042 for rec, 1.20 for cir
+	area := s.Area()
+	switch s.(type) {
+	case Square:
+		rate := 0.02
+		return area * rate
+
+	case Rectangle:
+		rate := 0.42
+		return area * rate
+
+	case Circle:
+		rate := 1.20
+		return area * rate
+
+	default:
+		rate := 1.00
+		return area * rate
+	}
 }
